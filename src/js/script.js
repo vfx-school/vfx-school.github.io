7531597c5530108b43044b3035a5cb891846b4ee
Gleb Smirnov <me@gvsmirnov.ru>
2013-10-24 15:49:28 +0400

    Fixed scrolling, alignment and colors

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,31 @@
-var currentPage = 1;
+$(function() {
+    var SCROLL_DURATION = 500;
+    var currentPage = 1;
 
-$('#body').on('activate.bs.scrollspy', function (e) {
-    activatePage($(e.target).data("x-page"));
+    $('#body').on('activate.bs.scrollspy', function (e) {
+        activatePage($(e.target).data("x-page"));
+    });
+
+    var activatePage = function(pageNumber) {
+        $('#body').removeClass("page-" + currentPage).addClass("page-" + pageNumber);
+        currentPage = pageNumber;
+    }
+
+    activatePage(currentPage);
+
+    var scrollTo = function(pageId) {
+        $.scrollTo($(pageId), SCROLL_DURATION);
+        return false;
+    }
+
+    $(".navbar-nav > li > a").each(function(index, element) {
+        element = $(element);
+        var hash = element[0].hash;
+
+        element.click(function(hash) {return function(e) {
+            e.preventDefault();
+            return scrollTo(hash);
+        }}(hash));
+    });
 });
 
-var activatePage = function(pageNumber) {
-    $('#body').removeClass("page-" + currentPage).addClass("page-" + pageNumber);
-    currentPage = pageNumber;
-}
-
-activatePage(1);
